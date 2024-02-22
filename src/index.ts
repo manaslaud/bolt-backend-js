d78f4fd8741eef9e16d5a229a6265040b10f4526
woaitsAryan <yoprince.ab@gmail.com>
2024-02-23 02:53:41 +0530

    fix: increased body limit

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const PORT = getEnv.PORT;
 connectToDB();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(helmet());
 
 if (getEnv.ENVIRONMENT === 'dev') app.use(morgan('dev'));
